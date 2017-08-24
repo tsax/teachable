8322490cca0e856bb479ad0175f6b98bd493da60
Tushar S <tsaxena86@gmail.com>
2017-08-24 02:13:18 -0400

    fix indentation

--- lib/teachable.rb
+++ lib/teachable.rb
@@ -9,34 +9,34 @@ module Teachable
     attr_reader :id, :name, :nickname, :image, :email, :tokens, :created_at, :updated_at
 
     def self.authenticate(email, password)
-    	response = connection.post '/users/sign_in.json',
-    						 						{
-    						 							user:
-    						 								{
-    						 									email: email,
-    						 							 		password: password
-    						 								}
-    						 						}
-    	body = JSON.parse(response.body)
+      response = connection.post '/users/sign_in.json',
+                                 {
+                                   user:
+                                    {
+                                      email: email,
+                                      password: password
+                                    }
+                                 }
+      body = JSON.parse(response.body)
 
       if (error = body["errors"] || body["error"])
         raise "Error: #{error}"
       end
-      
+
       return new(body)
     end
 
     def self.register_new_user(email, password, password_confirmation)
       raise "Error: password and password_confirmation must match" unless password == password_confirmation
-    	response = connection.post '/users.json',
-    												{
-    													user:
-    													{
-    														email: email,
-    														password: password,
-    														password_confirmation: password_confirmation
-    													}
-    												}
+      response = connection.post '/users.json',
+        {
+        user:
+        {
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation
+      }
+      }
       body = JSON.parse(response.body)
 
       if (error = body["errors"] || body["error"])
@@ -47,13 +47,13 @@ module Teachable
     end
 
     def get_user_details
-    	response = connection.get '/api/users/current_user/edit.json',
-    												{
-    													user_email: self.email,
-    													user_token: self.tokens
-    												}
+      response = connection.get '/api/users/current_user/edit.json',
+        {
+        user_email: self.email,
+        user_token: self.tokens
+      }
       body = JSON.parse(response.body)
-        
+
       if (error = body["errors"] || body["error"])
         raise "Error: #{body["errors"]}"
       end
@@ -62,12 +62,12 @@ module Teachable
     end
 
     def get_orders_for_user
-    	response = connection.get '/api/orders.json',
-    												{
-    													user_email: self.email,
-    													user_token: self.tokens
-    												}
-    	body = JSON.parse(response.body)
+      response = connection.get '/api/orders.json',
+        {
+        user_email: self.email,
+        user_token: self.tokens
+      }
+      body = JSON.parse(response.body)
 
       if (body.class != Array && error = body["errors"] || body["error"])
         raise "Error: #{error}"
@@ -81,28 +81,31 @@ module Teachable
     end
 
     def create_order_for_user(total, total_quantity)
-    	response = connection.post "/api/orders.json?user_email=#{self.email}&user_token=#{self.tokens}",
-    												{
-    													order:
-    													{
-    														total: total,
-    														total_quantity: total_quantity,
-    														email: self.email
-    													}
-    												}
-      unless body["errors"]
+      response = connection.post "/api/orders.json?user_email=#{self.email}&user_token=#{self.tokens}",
+        {
+          order:
+          {
+            total: total,
+            total_quantity: total_quantity,
+            email: self.email
+      }
+      }
+
+      body = JSON.parse(response.body)
+
+      if (error = body["errors"] || body["error"])
+        raise "Error: #{body["errors"]}"
+      else
         return Order.new(body)
       end
-
-      return JSON.parse(response.body)
     end
 
     def delete_order_for_user(order_id)
-    	response = connection.delete "/api/orders/#{order_id}.json",
-    												{
-    													user_email: self.email,
-    													user_token: self.tokens
-    												}
+      response = connection.delete "/api/orders/#{order_id}.json",
+        {
+        user_email: self.email,
+        user_token: self.tokens
+      }
       if response.body.empty?
         return "Order:#{order_id} deleted!"
       else
@@ -133,18 +136,18 @@ module Teachable
   end
 
   class Order
-  	attr_reader :id, :email, :number, :total, :total_quantity,
-  							:special_instructions, :created_at, :updated_at
+    attr_reader :id, :email, :number, :total, :total_quantity,
+      :special_instructions, :created_at, :updated_at
 
-  	def initialize(attributes)
-  		@id = attributes["id"]
-  		@email = attributes["email"]
-  		@number = attributes["number"]
-  		@total = attributes["total"]
-  		@total_quantity = attributes["total_quantity"]
-  		@special_instructions = attributes["special_instructions"]
-  		@created_at = attributes["created_at"]
-  		@updated_at = attributes["updated_at"]
-  	end
+    def initialize(attributes)
+      @id = attributes["id"]
+      @email = attributes["email"]
+      @number = attributes["number"]
+      @total = attributes["total"]
+      @total_quantity = attributes["total_quantity"]
+      @special_instructions = attributes["special_instructions"]
+      @created_at = attributes["created_at"]
+      @updated_at = attributes["updated_at"]
+    end
   end
 end
--- spec/teachable_spec.rb
+++ spec/teachable_spec.rb
@@ -8,97 +8,115 @@ RSpec.describe Teachable do
   end
 
   describe "register new user" do
-  	let(:user_email) 		{ "whatever22@example.com" }
-  	let(:user_password) { "password" }
+    let(:user_email) 		{ "whatever22@example.com" }
+    let(:user_password) { "password" }
 
-		it "when password and password_confirmation don't match" do
-  		expect{ Teachable::User.register_new_user(user_email, user_password, "random") }
-  		.to raise_error('Error: password and password_confirmation must match')
-  	end
+    it "when password and password_confirmation don't match" do
+      expect{ Teachable::User.register_new_user(user_email, user_password, "random") }
+        .to raise_error('Error: password and password_confirmation must match')
+    end
 
-  	it "when using existing email, throws an error" do
-  		VCR.use_cassette('new_user_error') do
-  			expect{ Teachable::User.register_new_user(user_email,
-  																							 user_password,
-  																							 user_password) }
-  			.to raise_error('Error: {"email"=>["has already been taken"]}')
-  		end
-  	end
+    it "when using existing email, throws an error" do
+      VCR.use_cassette('new_user_error') do
+        expect{ Teachable::User.register_new_user(user_email,
+                                                  user_password,
+                                                  user_password) }
+          .to raise_error('Error: {"email"=>["has already been taken"]}')
+      end
+    end
 
-  	context "when using a new email" do
-  		let(:user_email) 		{ "new_email11@example.com" }
-  		let(:user_password)	{ "password" }
+    context "when using a new email" do
+      let(:user_email) 		{ "new_email11@example.com" }
+      let(:user_password)	{ "password" }
 
-  		it "returns a new user" do
-  			VCR.use_cassette('new_user') do
-  				user = Teachable::User.register_new_user(user_email,
-  					user_password,
-  					user_password)
-  				puts user
-  				expect(user.class).to eq(Teachable::User)
-  				expect(user.email).to eq(user_email)
-  			end
-  		end
-  	end
+      it "returns a new user" do
+        VCR.use_cassette('new_user') do
+          user = Teachable::User.register_new_user(user_email,
+                                                   user_password,
+                                                   user_password)
+          puts user
+          expect(user.class).to eq(Teachable::User)
+          expect(user.email).to eq(user_email)
+        end
+      end
+    end
   end
 
   describe "authenticate existing user" do
-  	let(:user_email) 		{ "whatever22@example.com" }
-  	let(:user_password) { "password" }
+    let(:user_email) 		{ "whatever22@example.com" }
+    let(:user_password) { "password" }
 
-  	it "returns a User object when login is successful" do
-  		VCR.use_cassette('authenticate_success') do
-  			user = Teachable::User.authenticate(user_email, user_password)
+    it "returns a User object when login is successful" do
+      VCR.use_cassette('authenticate_success') do
+        user = Teachable::User.authenticate(user_email, user_password)
 
-  			expect(user.class).to eq(Teachable::User)
-  			expect(user.email).to eq(user_email)
-  			expect(user.tokens).not_to eq(nil)
-  		end
-  	end
+        expect(user.class).to eq(Teachable::User)
+        expect(user.email).to eq(user_email)
+        expect(user.tokens).not_to eq(nil)
+      end
+    end
 
-  	context "when login is unsuccessful" do
-  		let(:user_email) 		{ "whatever22@example.com" }
-  		let(:user_password) { "password1" }
+    context "when login is unsuccessful" do
+      let(:user_email) 		{ "whatever22@example.com" }
+      let(:user_password) { "password1" }
 
-  		it "raises an error" do
-  			VCR.use_cassette('authenticate_error') do
-  				expect { Teachable::User.authenticate(user_email, user_password) }
-  				.to raise_error("Error: Invalid email or password.")
-  			end
-  		end
-  	end
+      it "raises an error" do
+        VCR.use_cassette('authenticate_error') do
+          expect { Teachable::User.authenticate(user_email, user_password) }
+            .to raise_error("Error: Invalid email or password.")
+        end
+      end
+    end
   end
 
   describe "get details of existing user" do
-		let(:user_email) 		{ "whatever22@example.com" }
-		let(:user_password) { "password" }
+    let(:user_email) 		{ "whatever22@example.com" }
+    let(:user_password) { "password" }
 
-		it "raises an error" do
-			VCR.use_cassette('user_details') do
-				user = Teachable::User.authenticate(user_email, user_password)
-				user_copy = user.get_user_details
+    it "raises an error" do
+      VCR.use_cassette('user_details') do
+        user = Teachable::User.authenticate(user_email, user_password)
+        user_copy = user.get_user_details
 
-				expect(user.email).to eq(user_copy.email)
-				expect(user.tokens).to eq(user_copy.tokens)
-			end
-		end
+        expect(user.email).to eq(user_copy.email)
+        expect(user.tokens).to eq(user_copy.tokens)
+      end
+    end
   end
 
   describe "get orders for user" do
-  	let(:user_email) 		{ "tsax@example.com" }
-		let(:user_password) { "password" }
+    let(:user_email) 		{ "tsax@example.com" }
+    let(:user_password) { "password" }
 
-		before do
-			VCR.use_cassette('tsax_user') do
-				@user = Teachable::User.authenticate(user_email, user_password)
-			end
-		end
-		it "gets orders" do
-			VCR.use_cassette('user_orders') do
-				orders = @user.get_orders_for_user
-				expect(orders.class).to eq(Array)
-			end
-		end
+    let(:u_email) { "dev-11111@example.com" }
+    let(:u_password) { "password" }
+
+    before do
+      VCR.use_cassette('tsax_user') do
+        @user = Teachable::User.authenticate(user_email, user_password)
+      end
+
+      VCR.use_cassette('no_orders_user') do
+        @no_orders_user = Teachable::User.authenticate(u_email, u_password)
+      end
+    end
+
+    it "when user has orders, it gets orders" do
+      VCR.use_cassette('user_orders') do
+        orders = @user.get_orders_for_user
+        expect(orders.class).to eq(Array)
+      end
+    end
+
+    context 'when user does not have any orders' do
+      it "returns an empty array" do
+        VCR.use_cassette('no_orders') do
+          orders = @no_orders_user.get_orders_for_user
+          expect(orders).to eq([])
+        end
+      end
+    end
+
   end
 
   describe "create an order for user" do
